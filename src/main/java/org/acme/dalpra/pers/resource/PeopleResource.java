ee2bcf42dfc265e9f86a802b675729fcc7099d9c
d.dalpra <d.dalpra@guttadauro.it>
2025-01-03 11:54:59 +0100

    cambio gestione campi standard con ricodifica di alcune parti.

--- src/main/java/org/acme/dalpra/pers/resource/PeopleResource.java
+++ src/main/java/org/acme/dalpra/pers/resource/PeopleResource.java
@@ -4,6 +4,8 @@ import io.quarkus.hibernate.orm.rest.data.panache.PanacheRepositoryResource;
 import org.acme.dalpra.pers.entity.Person;
 import org.acme.dalpra.pers.repos.PersonRepository;
 
-public interface PeopleResource extends PanacheRepositoryResource<PersonRepository, Person, Long> {
+import java.util.UUID;
+
+public interface PeopleResource extends PanacheRepositoryResource<PersonRepository, Person, UUID> {
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 -- Repository sample
-INSERT INTO personForRepository(name, birthDate) VALUES ('John Johnson', '1988-01-10');
-INSERT INTO personForRepository(name, birthDate) VALUES ('Peter Peterson', '1986-11-20');
+INSERT INTO personForRepository(id,name, birthDate) VALUES ('3046c823-8b7b-401e-853c-d085b67c391b','John Johnson', '1988-01-10');
+INSERT INTO personForRepository(id,name, birthDate) VALUES ('aa1aade5-2b63-4abb-8fdc-e7f7d994b491','Peter Peterson', '1986-11-20');
