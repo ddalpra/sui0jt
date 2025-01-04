b6191970d432a1348a5a945ee57882b7d7d9364d
ddalpra <d.dalpra86@gmail.com>
2025-01-04 10:16:51 +0100

    Sistemazioni

--- src/main/java/org/acme/dalpra/pers/repos/PersonRepository.java
+++ src/main/java/org/acme/dalpra/pers/repos/PersonRepository.java
@@ -3,7 +3,6 @@ package org.acme.dalpra.pers.repos;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
